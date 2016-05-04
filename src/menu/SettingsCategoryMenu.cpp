4435b3988f0f1970ea19dd4c79f59813b6209e52
MasQchips <masqchips@gmail.com>
2016-05-05 01:26:08 +0200

    Added GUI Language
    Update Language
    Added Swedish

--- src/menu/SettingsCategoryMenu.cpp
+++ src/menu/SettingsCategoryMenu.cpp
@@ -20,21 +20,23 @@
 #include "utils/StringTools.h"
 #include "KeyPadMenu.h"
 #include "ButtonChoiceMenu.h"
+#include "SettingsLanguageMenu.h"
 #include "language/gettext.h"
 
 #define MAX_SETTINGS_PER_PAGE 3
 
-SettingsCategoryMenu::SettingsCategoryMenu(int w, int h, const std::string & title, const SettingType * catSettings, int settingsCount)
+SettingsCategoryMenu::SettingsCategoryMenu(int w, int h, const std::string & title, const char *nameTitleImage, const SettingType * catSettings, int settingsCount)
     : GuiFrame(w, h)
     , categorySettings(catSettings)
     , categorySettingsCount(settingsCount)
+	, categoryNameTitle(nameTitleImage)
     , categoryFrame(w, h)
-    , scrollbar(h - 150)
+	, scrollbar(h - 150)
     , buttonClickSound(Resources::GetSound("settings_click_2.mp3"))
     , backImageData(Resources::GetImageData("backButton.png"))
     , backImage(backImageData)
     , backButton(backImage.getWidth(), backImage.getHeight())
-    , titleImageData(Resources::GetImageData("settingsTitle.png"))
+    , titleImageData(Resources::GetImageData(categoryNameTitle))
     , titleImage(titleImageData)
     , settingImageData(Resources::GetImageData("settingButton.png"))
     , settingSelectedImageData(Resources::GetImageData("settingSelectedButton.png"))
@@ -50,7 +52,7 @@ SettingsCategoryMenu::SettingsCategoryMenu(int w, int h, const std::string & tit
 {
     currentSettingsIdx = 0;
     currentYOffset = 0;
-
+	
     backButton.setImage(&backImage);
     backButton.setAlignment(ALIGN_BOTTOM | ALIGN_LEFT);
     backButton.clicked.connect(this, &SettingsCategoryMenu::OnBackButtonClick);
@@ -72,14 +74,20 @@ SettingsCategoryMenu::SettingsCategoryMenu(int w, int h, const std::string & tit
     for(int i = 0; i < categorySettingsCount; i++)
     {
         settings[i].settingImage = new GuiImage(settingImageData);
+		settings[i].settingIcon = new GuiImage(Resources::GetImageData(categorySettings[i].icon));
+		settings[i].settingIconOver = new GuiImage(Resources::GetImageData(categorySettings[i].iconGlow));
         settings[i].settingImageSelected = new GuiImage(settingSelectedImageData);
         settings[i].settingButton = new GuiButton(settingImageData->getWidth(), settingImageData->getHeight());
         settings[i].settingLabel = new GuiText(tr(categorySettings[i].name), 46, glm::vec4(0.8f, 0.8f, 0.8f, 1.0f));
 
-        settings[i].settingButton->setIconOver(settings[i].settingImageSelected);
-        settings[i].settingButton->setImage(settings[i].settingImage);
+        settings[i].settingButton->setImageSelectOver(settings[i].settingImageSelected);
+		settings[i].settingButton->setIcon(settings[i].settingIcon);
+		settings[i].settingButton->setIconOver(settings[i].settingIconOver);
+		settings[i].settingButton->setImage(settings[i].settingImage);
         settings[i].settingButton->setLabel(settings[i].settingLabel);
 
+		settings[i].settingIcon->setPosition(-300, 0);
+		settings[i].settingIconOver->setPosition(-300, 0);
         settings[i].settingButton->setPosition(0, 150 - (settings[i].settingImage->getHeight() + 30) * i);
         settings[i].settingButton->setTrigger(&touchTrigger);
         settings[i].settingButton->setTrigger(&wpadTouchTrigger);
@@ -122,6 +130,8 @@ SettingsCategoryMenu::~SettingsCategoryMenu()
     for(u32 i = 0; i < settings.size(); ++i)
     {
         delete settings[i].settingImage;
+		delete settings[i].settingIcon;
+		delete settings[i].settingIconOver;
         delete settings[i].settingImageSelected;
         delete settings[i].settingButton;
         delete settings[i].settingLabel;
@@ -161,6 +171,17 @@ void SettingsCategoryMenu::OnSettingButtonClick(GuiButton *button, const GuiCont
             menu = keyMenu;
             break;
         }
+		case TypeDownloadLanguage:
+		case TypeSelectLanguage: {
+			if(CSettings::getDataType(categorySettings[i].index) != CSettings::TypeString)
+                return;
+			
+			SettingsLanguageMenu *LanguageMenu = new SettingsLanguageMenu(width, height, tr(categorySettings[i].name), categoryNameTitle);
+			LanguageMenu->settingsBackClicked.connect(this, &SettingsCategoryMenu::OnSubMenuCloseClicked);
+			menu = LanguageMenu;
+			break;
+			
+        }
         case Type2Buttons:
         case Type3Buttons:
         case Type4Buttons: {
@@ -198,8 +219,8 @@ void SettingsCategoryMenu::OnSettingButtonClick(GuiButton *button, const GuiCont
             std::vector<std::string> buttonNames;
             for(int n = 0; n < buttonCount; n++)
                 buttonNames.push_back(tr(categorySettings[i].valueStrings[n].name));
-
-            ButtonChoiceMenu *buttonMenu = new ButtonChoiceMenu(width, height, tr(categorySettings[i].name), buttonNames, buttonSelected);
+				
+            ButtonChoiceMenu *buttonMenu = new ButtonChoiceMenu(width, height, tr(categorySettings[i].name), categoryNameTitle, buttonNames, buttonSelected);
             buttonMenu->settingsBackClicked.connect(this, &SettingsCategoryMenu::OnSubMenuCloseClicked);
             buttonMenu->settingsOkClicked.connect(this, &SettingsCategoryMenu::OnButtonChoiceOkClicked);
             menu = buttonMenu;
---
+++ src/settings/CSettingsLanguages.cpp
@@ -0,0 +1,106 @@
+/****************************************************************************
+ * Copyright (C) 2015 Dimok
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#include <map>
+#include <string.h>
+#include "common/common.h"
+#include "CSettingsLanguages.h"
+#include "utils/StringTools.h"
+#include "CSettings.h"
+#include "fs/DirList.h"
+#include "language/gettext.h"
+
+CSettingsLanguages *CSettingsLanguages::instance = NULL;
+
+CSettingsLanguages::CSettingsLanguages()
+{
+	this->languagesPath = SD_PATH  WIIU_PATH "/apps/loadiine_gx2/languages";
+	this->filter = ".lang";
+}
+
+CSettingsLanguages::~CSettingsLanguages()
+{
+	/*
+    for(u32 i = 0; i < settingsGames.size(); i++){
+		for(u32 j = 0; j < settingsGames[i].settingsValues.size(); j++){
+			if(settingsGames[i].settingsValues.at(j).dataType == TypeString)
+				delete settingsGames[i].settingsValues.at(j).strValue;
+		}
+    }*/
+}
+
+std::vector<std::string> CSettingsLanguages::LoadLanguages(int *languageSelect)
+{
+	DirList dirList;			
+	
+	std::string appLanguage(CSettings::getValueAsString(CSettings::AppLanguage));
+	std::vector<std::string> languageName;
+	
+	typedef std::map<std::string, std::map<std::string, std::string > > languagesMap;
+
+	languagesMap languagesNames;
+
+	languagesNames["brazilian.lang"]["brazilian"] = trNOOP("Brazilian");
+	languagesNames["chinese.lang"]["chinese"] =  trNOOP("Chinese");
+	languagesNames["chinese_T.lang"]["chinese_T"] =  trNOOP("Traditional Chinese");
+	languagesNames["english.lang"]["english"] =  trNOOP("English");
+	languagesNames["french.lang"]["french"] =  trNOOP("French");
+	languagesNames["german.lang"]["german"] =  trNOOP("German");
+	languagesNames["italian.lang"]["italian"] =  trNOOP("Italian");
+	languagesNames["spanish.lang"]["spanish"] =  trNOOP("Spanish");
+	languagesNames["swedish.lang"]["swedish"] =  trNOOP("Swedish");
+	
+	enumLanguages.insert(std::pair<int, std::string>(languageName.size(), "default"));
+	languageName.push_back("Default");	
+	if (appLanguage == "")
+		*languageSelect = 0;
+	
+	if(dirList.LoadPath(languagesPath, filter))
+	{				
+		for(int i = 0; i < dirList.GetFilecount(); i++) {
+			for (languagesMap::iterator nameFile = languagesNames.begin(); nameFile != languagesNames.end(); ++nameFile) {
+				if(dirList.GetFilename(i) == nameFile->first)
+				{
+					for (std::map<std::string, std::string >::iterator nameLanguage = nameFile->second.begin(); nameLanguage != nameFile->second.end(); ++nameLanguage) {						
+						if (appLanguage == nameLanguage->first) 
+							*languageSelect = languageName.size();
+						enumLanguages.insert(std::pair<int, std::string>(languageName.size(), nameLanguage->first));
+						languageName.push_back(nameLanguage->second);
+					}
+				}
+			}
+		}
+	}
+
+	languagesNames.clear();
+	return languageName;
+	
+}
+void CSettingsLanguages::SetLanguage(int languageSelect)
+{
+	std::string nameLanguage;
+	for ( std::map<int,std::string>::iterator nameSetLanguage = enumLanguages.begin() ; nameSetLanguage != enumLanguages.end(); nameSetLanguage++ ){
+		if (nameSetLanguage->first == languageSelect){
+			nameSetLanguage->second == "default"? nameLanguage = "" : nameLanguage = nameSetLanguage->second ;
+			break;			
+		}
+	}
+	
+	CSettings::setValueAsString(CSettings::AppLanguage, nameLanguage);
+	//! load language
+	languagesPath = "sd:/wiiu/apps/loadiine_gx2/languages/" + CSettings::getValueAsString(CSettings::AppLanguage) + ".lang";
+	gettextLoadLanguage(languagesPath.c_str());
+}
